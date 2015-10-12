1461f2dac52bb563126b15424ebb7408138951d8
u3s <czesacz@gmail.com>
2015-10-12 16:32:59 +0200

    [task] tc_02 added

--- lights/test/lights_SUITE.erl
+++ lights/test/lights_SUITE.erl
@@ -1,9 +1,37 @@
 -module(lights_SUITE).
 -include_lib("common_test/include/ct.hrl").
-%-include("../include/tl.hrl").
--export([all/0,tc_eunit/1]).
+-export([all/0, tc01_eunit/1, tc02_simple_crossroad/1, groups/0, init_per_group/2, end_per_group/2]).
 
-all() -> [tc_eunit].
+all() -> [{group, eunit}, {group, integration}].
 
-tc_eunit(_Config) ->
+groups() -> [{eunit, [], [tc01_eunit]}, 
+			 {integration, [], [tc02_simple_crossroad]}].
+
+tc01_eunit(_Config) ->
     ok = eunit:test(tl_tests, [verbose]).
+
+
+init_per_group(integration, Config) ->
+    ok = application:start(lights),
+    ct:pal(">>> lights application started"),
+    tl:add(),
+    tl:add(),
+    tl:add(),
+    tl:add(),
+    ct:pal(">>> robots added"),
+	Config;
+
+init_per_group(_, _Config) ->
+	_Config.
+
+end_per_group(integration, Config) ->
+	tl:remove_all(),
+	ct:pal(">>> robots removed"),
+    ok = application:stop(lights),
+    ct:pal(">>> lights application stopped"),
+	Config;
+end_per_group(_, Config) ->
+	ok.
+
+tc02_simple_crossroad(_Config) ->
+	ok.
